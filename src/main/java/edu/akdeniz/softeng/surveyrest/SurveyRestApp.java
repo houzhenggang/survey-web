79bf1a3dd4e3f571a727eb30472fb4f59f3a65c8
Emre SEN <maemresen07@gmail.com>
2018-06-08 13:27:43 +0300

    Web Application config - not stable.

--- src/main/java/edu/akdeniz/softeng/surveyrest/SurveyRestApp.java
+++ src/main/java/edu/akdeniz/softeng/surveyrest/SurveyRestApp.java
@@ -4,12 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @SpringBootApplication
-public class SurveyRestApp {
-    public static void main(String[] args) {
+public class SurveyRestApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SurveyRestApp.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SurveyRestApp.class, args);
     }
-}
 
+}
